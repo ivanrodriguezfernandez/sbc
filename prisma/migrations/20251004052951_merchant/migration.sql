1dffdeeb9fa7f3b605e6b1565e57eb94566e0333
Iván Rodríguez <ivanrodriguezfernandez@gmail.com>
2025-10-04 07:33:46 +0200

    add migration

---
+++ prisma/migrations/20251004052951_merchant/migration.sql
@@ -0,0 +1,29 @@
+-- CreateEnum
+CREATE TYPE "disbursementFrequencyType" AS ENUM ('WEEKLY', 'DAILY');
+
+-- CreateTable
+CREATE TABLE "merchant" (
+    "id" TEXT NOT NULL,
+    "reference" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" TEXT NOT NULL,
+    "reference" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "live_on" TIMESTAMP(3) NOT NULL,
+    "disbursement_frequency" "disbursementFrequencyType" NOT NULL,
+    "minimum_monthly_fee" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(6) NOT NULL,
+    "deletedAt" TIMESTAMPTZ(6),
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
