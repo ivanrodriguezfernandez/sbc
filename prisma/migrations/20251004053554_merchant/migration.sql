86cfdf0e0af6ffcd25b3d552942b589dfc6211cd
Iván Rodríguez <ivanrodriguezfernandez@gmail.com>
2025-10-04 07:37:50 +0200

    fix merchant schema creation

--- prisma/migrations/20251004053554_merchant/migration.sql
+++ prisma/migrations/20251004053554_merchant/migration.sql
@@ -6,15 +6,6 @@ CREATE TABLE "merchant" (
     "id" TEXT NOT NULL,
     "reference" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-
-    CONSTRAINT "merchant_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Merchant" (
-    "id" TEXT NOT NULL,
-    "reference" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
     "live_on" TIMESTAMP(3) NOT NULL,
     "disbursement_frequency" "disbursementFrequencyType" NOT NULL,
     "minimum_monthly_fee" DOUBLE PRECISION NOT NULL,
@@ -22,8 +13,8 @@ CREATE TABLE "Merchant" (
     "updatedAt" TIMESTAMPTZ(6) NOT NULL,
     "deletedAt" TIMESTAMPTZ(6),
 
-    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "merchant_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
+CREATE UNIQUE INDEX "merchant_email_key" ON "merchant"("email");
