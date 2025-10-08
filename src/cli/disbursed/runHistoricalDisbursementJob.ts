b833619dd5d3f00b1c7035d7c4a852f7485e6dce
Iván Rodríguez <ivanrodriguezfernandez@gmail.com>
2025-10-08 14:10:10 +0200

    Get weeks between start date and end date

--- src/cli/disbursed/runHistoricalDisbursementJob.ts
+++ src/cli/disbursed/runHistoricalDisbursementJob.ts
@@ -1,3 +1,5 @@
+import { addDays, addWeeks, getDay, isBefore, startOfWeek } from "date-fns";
+
 import { getDB } from "../../__shared__/infrastructure/db";
 
 export async function getDistinctTransactionDates(): Promise<Array<Date>> {
@@ -14,4 +16,30 @@ export async function getDistinctTransactionDates(): Promise<Array<Date>> {
 	});
 
 	return dates.map((d) => d.transactionDate);
+}
+
+export async function getDistinctWeeks(): Promise<Array<{ start: Date; end: Date }>> {
+	const prisma = getDB();
+	const result = await prisma.order.aggregate({
+		_min: {
+			transactionDate: true,
+		},
+		_max: {
+			transactionDate: true,
+		},
+	});
+	const minDate = result._min.transactionDate;
+	const maxDate = result._max.transactionDate;
+	const firstMonday = startOfWeek(minDate, { weekStartsOn: 1 });
+
+	const weeks: { start: Date; end: Date }[] = [];
+	let current = firstMonday;
+
+	while (isBefore(current, maxDate)) {
+		const start = current;
+		const end = addWeeks(start, 1);
+		weeks.push({ start, end });
+		current = end;
+	}
+	return weeks;
 }
--- tests/cli/disbursement/disbursement.test.ts
+++ tests/cli/disbursement/disbursement.test.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 import { createDbContext } from "../../../src/__shared__/infrastructure/prisma.extensions";
-import { getDistinctTransactionDates } from "../../../src/cli/disbursed/runHistoricalDisbursementJob";
+import {
+	getDistinctTransactionDates,
+	getDistinctWeeks,
+} from "../../../src/cli/disbursed/runHistoricalDisbursementJob";
 import { setupPostgresContainer, teardownPostgresContainer } from "../../config/setup";
 
 let prisma: PrismaClient;
@@ -22,10 +25,7 @@ describe("Historical Disbursement Job - Distinct Transaction Dates", () => {
 	});
 	it("should return only distinct transaction dates from orders", async () => {
 		const dbContext = createDbContext(prisma);
-		const merchant = await dbContext.merchant.create({
-			reference: "padberg_group",
-			liveOn: new Date("2023-02-01T00:00:00.000Z"),
-		});
+		const merchant = await dbContext.merchant.create({ reference: "padberg_group" });
 
 		await dbContext.order.create({
 			merchantId: merchant.id,
@@ -45,12 +45,49 @@ describe("Historical Disbursement Job - Distinct Transaction Dates", () => {
 		});
 
 		const data = await getDistinctTransactionDates();
+
+		const dataStrings = data.map((d) => d.toISOString());
+
 		const expected = [
-			new Date("2021-02-01T00:00:00.000Z"),
-			new Date("2023-02-01T00:00:00.000Z"),
-			new Date("2024-02-01T00:00:00.000Z"),
+			"2021-02-01T00:00:00.000Z",
+			"2023-02-01T00:00:00.000Z",
+			"2024-02-01T00:00:00.000Z",
 		];
 
-		expect(data).toStrictEqual(expected);
+		expect(dataStrings).toStrictEqual(expected);
+	});
+
+	it("should return only distinct week dates from orders", async () => {
+		const dbContext = createDbContext(prisma);
+		const merchant = await dbContext.merchant.create({ reference: "padberg_group" });
+
+		await dbContext.order.create({
+			merchantId: merchant.id,
+			transactionDate: new Date("2023-03-01T00:00:00.000Z"),
+		});
+		await dbContext.order.create({
+			merchantId: merchant.id,
+			transactionDate: new Date("2023-03-15T00:00:00.000Z"),
+		});
+		await dbContext.order.create({
+			merchantId: merchant.id,
+			transactionDate: new Date("2023-03-31T00:00:00.000Z"),
+		});
+
+		const data = await getDistinctWeeks();
+
+		const dataStrings = data.map((w) => ({
+			start: w.start.toISOString(),
+			end: w.end.toISOString(),
+		}));
+
+		const expected = [
+			{ start: "2023-02-26T23:00:00.000Z", end: "2023-03-05T23:00:00.000Z" },
+			{ start: "2023-03-05T23:00:00.000Z", end: "2023-03-12T23:00:00.000Z" },
+			{ start: "2023-03-12T23:00:00.000Z", end: "2023-03-19T23:00:00.000Z" },
+			{ start: "2023-03-19T23:00:00.000Z", end: "2023-03-26T22:00:00.000Z" },
+			{ start: "2023-03-26T22:00:00.000Z", end: "2023-04-02T22:00:00.000Z" },
+		];
+		expect(dataStrings).toStrictEqual(expected);
 	});
 });
